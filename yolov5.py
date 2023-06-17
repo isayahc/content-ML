415ca1ab7ad44f140dee50150f4b71c2f8e6eb44
isayahc <isayahc>
2023-06-16 20:14:02 -0400

    segement video to where there are people

--- yolov5.py
+++ yolov5.py
@@ -1,18 +1,41 @@
 import cv2
 import torch
+import os
 from tqdm import tqdm
 
+# set flag
+store_only_people = True  # set this flag to False if you want to store images without people
+
+# load model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+# create directory to store frames if not exist
+output_dir = "assets\output_frames"
+os.makedirs(output_dir, exist_ok=True)
 
 cap = cv2.VideoCapture('video.mp4') # open video file
 total_frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) # get total number of frames
+
 for i in tqdm(range(total_frame_count)): # loop over frames with tqdm
     cap.set(cv2.CAP_PROP_POS_FRAMES, i) # move to i-th frame
     ret, frame = cap.read() # read frame
     if ret: # if frame is valid
-        # frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB) # convert to RGB (remove this line)
         results = model(frame) # pass to model
-        results.print() # print results
+        # Select only person class detections
+        person_detections = [x for x in results.xyxy[0] if x[-1] == 0.0]
+        if person_detections: # Check if detections are not empty
+            for *box, _, _, class_id in person_detections:
+                # check if bounding box has all the four coordinates
+                if len(box) == 4:
+                    # draw the bounding box on the frame
+                    cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 2)
+            if store_only_people:
+                # save the frame with people as image file
+                cv2.imwrite(os.path.join(output_dir, f"frame_{i}.jpg"), frame)
+        else: # if no person detected
+            if not store_only_people:
+                # save the frame without people as image file
+                cv2.imwrite(os.path.join(output_dir, f"frame_{i}.jpg"), frame)
     else: # if frame is invalid
         break # exit loop
 
