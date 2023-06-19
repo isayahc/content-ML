82a2635ded71b5fb89b92afdc93f6d00b4c07a46
isayahc <isayahc>
2023-06-19 15:50:30 -0400

    added typing and documentation

--- segment_people.py
+++ segment_people.py
@@ -5,7 +5,16 @@ import os
 from tqdm import tqdm
 
 
-def save_roi_images(model, video_path, output_dir, frame_skip_threshold=25):
+def save_roi_images(model: torch.nn.Module, video_path: str, output_dir: str, frame_skip_threshold: int = 25) -> None:
+    """
+    This function performs person detection on a given video file using the provided model,
+    then saves regions of interest (ROI) as images in the specified output directory.
+
+    :param model: PyTorch model for person detection.
+    :param video_path: Path to the input video file.
+    :param output_dir: Path to the output directory where ROI images will be saved.
+    :param frame_skip_threshold: The number of frames to skip between detections.
+    """
 
     # check video file
     if not os.path.isfile(video_path):
@@ -45,7 +54,17 @@ def save_roi_images(model, video_path, output_dir, frame_skip_threshold=25):
 
     cap.release()
 
-def get_roi_dict(model, video_path, frame_skip_threshold=25):
+def get_roi_dict(model: torch.nn.Module, video_path: str, frame_skip_threshold: int = 25) -> Dict[str, List[np.array]]:
+    """
+    This function performs person detection on a given video file using the provided model,
+    then returns a dictionary where for every frame it stores a list of ROI values.
+
+    :param model: PyTorch model for person detection.
+    :param video_path: Path to the input video file.
+    :param frame_skip_threshold: The number of frames to skip between detections.
+    :return: A dictionary where the keys are 'frame_n' (n is the frame number)
+             and the values are lists of ROI values.
+    """
 
     # check video file
     if not os.path.isfile(video_path):
@@ -82,7 +101,11 @@ def get_roi_dict(model, video_path, frame_skip_threshold=25):
 
     return roi_dict
 
-def main():
+def main() -> None:
+    """
+    The main function loads the model and performs person detection on the input video file,
+    then either saves the ROI as images or gets a dictionary of ROI values for each frame in the video.
+    """
 
     # load model
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
@@ -97,7 +120,7 @@ def main():
         os.mkdir(output_dir)
 
     # function to perform person detection and save the ROIs as images
-    save_roi_images(model, video_path, output_dir)
+    # save_roi_images(model, video_path, output_dir)
 
     # function to return a dictionary of ROI values for each frame in a given video
     roi_dict = get_roi_dict(model, video_path)
