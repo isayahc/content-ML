8acaad5f69298b6760cad3ace38c1a831db6b4e8
isayahc <isayahc>
2023-06-19 23:46:07 -0400

    attempting to fix issues in code

--- alt_verify_faces1.py
+++ alt_verify_faces1.py
@@ -28,21 +28,16 @@ def get_clusters(embeddings: Dict[int, np.ndarray], algorithm: str = 'DBSCAN', p
         raise ValueError("Invalid algorithm choice. Choose 'DBSCAN' or 'HDBSCAN'.")
 
     # Prepare data for clustering
-
     embedding_list = [embedding for embedding in embeddings.values()]
-
-
-    embedding_array = np.array([embedding_dict['embedding'] for embedding_list_element in embedding_list for embedding_dict in embedding_list_element])
+    # embedding_array = np.array(embedding_list)
+    embedding_array = np.vstack(embedding_list)
     labels = clustering_model.fit_predict(embedding_array)
     
-    # embedding_array = np.array([embedding['embedding'] for embedding in embedding_list])
-    # labels = clustering_model.fit_predict(embedding_array)
-    # labels = clustering_model.fit_predict(embedding_list)
     return {img_idx: cluster_label for img_idx, cluster_label in enumerate(labels)}
 
-def process_directory(directory_path: str, algorithm: str = 'DBSCAN', params: dict = None):
+def process_directory(input_dir: str, output_dir: str, algorithm: str = 'DBSCAN', params: dict = None):
     # Get all image paths in the directory
-    image_paths = [os.path.join(directory_path, filename) for filename in os.listdir(directory_path) if filename.endswith(".jpg") or filename.endswith(".png")]
+    image_paths = [os.path.join(input_dir, filename) for filename in os.listdir(input_dir) if filename.endswith(".jpg") or filename.endswith(".png")]
 
     # Calculate embeddings for all images
     embeddings = get_deepface_embeddings(image_paths)
@@ -53,10 +48,20 @@ def process_directory(directory_path: str, algorithm: str = 'DBSCAN', params: di
     # Save images in clusters
     common_indices = set(image_paths).intersection(set(clusters.keys()))
     for img_idx in common_indices:
-        cluster_dir = os.path.join(directory_path, f"person{clusters[img_idx] + 1}")
+        cluster_dir = os.path.join(output_dir, f"person{clusters[img_idx] + 1}")
         os.makedirs(cluster_dir, exist_ok=True)
         shutil.copy(image_paths[img_idx], cluster_dir)
 
+def main():
+    input_directory = "/home/isayahc/projects/machine_learning/facial_recognition/deepface_inference/assets/video1"
+    output_directory = "/home/isayahc/projects/machine_learning/facial_recognition/deepface_inference/assets/video2"
+    process_directory(input_directory, output_directory, 'DBSCAN', params={'eps': 0.3, 'min_samples': 5})
+    process_directory(input_directory, output_directory, 'HDBSCAN', params={'min_cluster_size': 5})
 
-process_directory('./assets/output_frames', 'DBSCAN', params={'eps': 0.3, 'min_samples': 5})
-process_directory('./assets/output_frames', 'HDBSCAN', params={'min_cluster_size': 5})
+if __name__ == '__main__':
+    main()
+
+
+
+
+
